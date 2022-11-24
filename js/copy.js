fb3942160d7dc05f4c34a1fa48a9828360786ca7
yaleiyale <yaleiyale@users.noreply.github.com>
2022-11-24 02:28:42 +0000

    deploy: yaleiyale/atlasblog@cd446f1528992981ae39e905adf634c85f01ab78

---
+++ js/copy.js
@@ -0,0 +1,36 @@
+(function () {
+
+    function flashCopyMessage(el, msg) {
+        el.textContent = msg;
+        setTimeout(function () {
+            el.textContent = "Copy";
+        }, 1000);
+    }
+
+    function toClip(el, text) {
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                flashCopyMessage(el, 'Copied😉')
+            })
+            .catch(err => {
+                console.log('copy went wrong', err);
+                flashCopyMessage(el, 'Failed😱')
+            });
+    }
+
+    function addCopyButton(containerEl) {
+        var copyBtn = document.createElement("button");
+        copyBtn.className = "highlight-copy-btn";
+        copyBtn.textContent = "Copy";
+
+        var codeEl = containerEl.firstElementChild;
+        copyBtn.addEventListener('click', function () {
+            toClip(this, codeEl.innerText);
+        });
+        containerEl.appendChild(copyBtn);
+    }
+
+    // Add copy button to code blocks
+    var highlightBlocks = document.getElementsByClassName('highlight');
+    Array.prototype.forEach.call(highlightBlocks, addCopyButton);
+})();
